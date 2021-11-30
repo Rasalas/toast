5c81dbe53a40202aaf14aa1bb31022585cf18524
Torben Buck <7483565+Rasalas@users.noreply.github.com>
2021-11-30 16:33:08 +0000

    ✨ toast

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
-let toast = new Toast({
-    message: 'Button clicked!',
+let toast_short = new Toast({
+    message: 'Yep, works!',
     type: 'success',
     position: 'top',
     duration: 'short'
 });
 
+let toast_long = new Toast({
+    message: 'This might take longer to read!',
+    type: 'warning',
+    position: 'bottom',
+    duration: 'long',
+})
+
 document.onreadystatechange = function () {
     if (document.readyState === "interactive") {
         
-        let button = document.querySelector("button");
-        button.addEventListener("click", function () {
-            toast.show();
+        let button_short = document.querySelector("#button_short");
+        button_short.addEventListener("click", function () {
+            toast_short.show();
+        });
+
+        let button_long = document.querySelector("#button_long");
+        button_long.addEventListener("click", function () {
+            toast_long.show();
         });
 
     }
